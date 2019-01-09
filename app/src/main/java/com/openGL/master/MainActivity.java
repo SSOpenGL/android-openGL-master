722c6ce5e3009e3c3965198bcbdd9709038f59f4
Suresh Chandak <suresh.chandak@paynearby.in>
2019-01-09 16:21:58 +0530

    create first openGL code

--- app/src/main/java/com/openGL/master/MainActivity.java
+++ app/src/main/java/com/openGL/master/MainActivity.java
@@ -5,9 +5,25 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private OpenGLView mOpenGLView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mOpenGLView = findViewById(R.id.openGLView);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mOpenGLView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mOpenGLView.onPause();
     }
 }
